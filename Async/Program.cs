31ceaf4593c5c48e5ef99b5fed6f443d76f57375
Joakim Olofsson <joakim.olofsson@iths.se>
2024-10-17 16:16:21 +0200

    Online exercises done

--- Async/Program.cs
+++ Async/Program.cs
@@ -2,9 +2,36 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            LongProcess();
+            ShortProcess();
+
+            Task<int> result = AnotherLongProcess();
+            Console.WriteLine($"Result: {await result}");
+
+            Console.ReadLine();
+        }
+
+        static async Task<int> AnotherLongProcess()
+        {
+            Console.WriteLine("Another Long process start!");
+            await Task.Delay(2000);
+            Console.WriteLine("Another Long process complete!");
+            return 10;
+        }
+
+        static async void LongProcess()
+        {
+            Console.WriteLine("Long process start!");
+            await Task.Delay(2000);
+            Console.WriteLine("Long process complete!");
+        }
+
+        static void ShortProcess()
+        {
+            Console.WriteLine("Short process start!");
+            Console.WriteLine("Short process complete!");
         }
     }
 }
